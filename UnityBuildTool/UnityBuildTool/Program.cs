995dc8555cc99d0595ad347c6d05b4d73720082c
kaka <superkaka.org@gmail.com>
2018-11-02 16:55:45 +0800

    unity build tool

---
+++ UnityBuildTool/UnityBuildTool/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using KLib;
+
+namespace UnityBuildTool
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("CurrentDirectory:" + Environment.CurrentDirectory);
+#if !DEBUG
+            try
+#endif
+            {
+                var dic = CommandParse.parse(args);
+
+                var command = dic["command"];
+
+                if (command == "processBuildInfo")
+                {
+                    var buildInfoPath = dic["buildInfoPath"];
+                    var tamplateDir = dic["tamplateDir"];
+
+                    BuildVersionProcesser.Process(buildInfoPath, tamplateDir);
+                }
+            }
+#if !DEBUG
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                Console.ReadLine();
+            }
+#endif
+        }
+    }
+}
