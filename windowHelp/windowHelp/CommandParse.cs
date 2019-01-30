447edacc0d7b6ae5aa198e1985a65285efae9af1
kaka <superkaka.org@gmail.com>
2019-01-30 16:34:02 +0800

    windowhelp

---
+++ windowHelp/windowHelp/CommandParse.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Collections;
+
+namespace windowHelp
+{
+    public class CommandParse
+    {
+
+        static public Dictionary<String, String> parse(String[] args)
+        {
+
+            var dic = new Dictionary<String, String>();
+            int i = 1;
+            while (i < args.Length - 1)
+            {
+                if (args[i].Substring(0, 1) == "-")
+                {
+                    dic[args[i].Substring(1)] = args[i + 1];
+                }
+                else
+                {
+                    dic[args[i]] = args[i + 1];
+                }
+
+                i += 2;
+            }
+
+            return dic;
+
+        }
+
+    }
+}
---
+++ windowHelp/windowHelp/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace windowHelp
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            var args = Environment.GetCommandLineArgs();
+            var dic = CommandParse.parse(args);
+            var content = dic["content"];
+            content = Encoding.UTF8.GetString(Convert.FromBase64String(content));
+            txt_content.IsReadOnly = true;
+            txt_content.Text = content;
+        }
+    }
+}
